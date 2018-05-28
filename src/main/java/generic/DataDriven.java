35825bd5a0201d0bc09f8aa023eacda6eb852fc4
Dharipenguin <Dharipenguin@Dharipenguin-PC>
2018-05-28 13:47:14 +0530

    2nd commit

--- src/main/java/generic/DataDriven.java
+++ src/main/java/generic/DataDriven.java
@@ -1,5 +1,30 @@
 package generic;
 
-public class DataDriven {
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class DataDriven implements Constant
+
+{
+	
+	
+	public String abc(String Sheet, int r, int c)
+	{
+	String s="";
+	try
+	{
+	FileInputStream f=new FileInputStream(PATH);
+		Workbook w = WorkbookFactory.create(f);
+		s=w.getSheet(Sheet).getRow(r).getCell(c).getStringCellValue();
+	}
+	catch(Exception e)
+	{
+		
+	}
+				
+	return s;			
+	}		
 
 }
